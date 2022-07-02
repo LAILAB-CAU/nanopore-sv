ffec328179d63315102901820477c4785021fde8
Mona Lisa <bxin@usc.edu>
2022-07-02 15:43:54 +0800

    init

--- SV genomic feature annotation/split_into_UTR_intron_exon_up2k.sh
+++ SV genomic feature annotation/split_into_UTR_intron_exon_up2k.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+## extract gene and exon
 perl get_gene_bed.pl Zm-Mo17-REFERENCE-CAU-1.0_Zm00014a.1.gff3  Mo17_CAU_gene_bed
 perl get_exon_bed_v2.pl Zm-Mo17-REFERENCE-CAU-1.0_Zm00014a.1.gff3  Mo17_CAU_exon_bed
 
+# extract exon from UTR
+
+
 cat Mo17_CAU_gene_bed  |perl -ne 'chomp;@ss=split;if($ss[4] eq "+"){$pos=$ss[1]-2000;$pos2=$ss[1]-1;if($pos>0){print "$ss[0]\t$pos\t$pos2\t$ss[3]\t$ss[4]\n";}else{print "$ss[0]\t0\t$pos2\t$ss[3]\t$ss[4]\n";}}if($ss[4] eq "-"){$pos=$ss[2]+2000;$pos2=$ss[2]+1;print "$ss[0]\t$pos2\t$pos\t$ss[3]\t$ss[4]\n";}' >maize_Mo17_up2K.bed
 
-cat Mo17_CAU_gene_bed  |perl -ne 'chomp;@ss=split;if($ss[4] eq "+"){$pos=$ss[2]+2000;$pos2=$ss[2]+1;print "$ss[0]\t$pos2\t$pos\t$ss[3]\t$ss[4]\n";}if($ss[4] eq "-"){$pos=$ss[1]-2000;$pos2=$ss[1]-1;if($pos>0){print "$ss[0]\t$pos\t$pos2\t$ss[3]\t$ss[4]\n";}else{print "$ss[0]\t0\t$pos2\t$ss[3]\t$ss[4]\n";}}' >maize_Mo17_down2K.bed
+# cat Mo17_CAU_gene_bed  |perl -ne 'chomp;@ss=split;if($ss[4] eq "+"){$pos=$ss[2]+2000;$pos2=$ss[2]+1;print "$ss[0]\t$pos2\t$pos\t$ss[3]\t$ss[4]\n";}if($ss[4] eq "-"){$pos=$ss[1]-2000;$pos2=$ss[1]-1;if($pos>0){print "$ss[0]\t$pos\t$pos2\t$ss[3]\t$ss[4]\n";}else{print "$ss[0]\t0\t$pos2\t$ss[3]\t$ss[4]\n";}}' >maize_Mo17_down2K.bed
 
-cat Mo17_CAU_gene_bed  |perl -ne 'chomp;@ss=split;$start=$ss[1]-2000;$end=$ss[2]+2000;if($start>0){print "$ss[0]\t$start\t$end\t$ss[3]\t$ss[4]\n";}' >maize_Mo17_Genes2K.bed
+# cat Mo17_CAU_gene_bed  |perl -ne 'chomp;@ss=split;$start=$ss[1]-2000;$end=$ss[2]+2000;if($start>0){print "$ss[0]\t$start\t$end\t$ss[3]\t$ss[4]\n";}' >maize_Mo17_Genes2K.bed
 
 cat Mo17_CAU_exon_bed  | sort -k4,4 -k1,1 -k2,2n > maize_Mo17_exon.bed.sorted
 cat maize_Mo17_exon.bed.sorted | awk '{print $1":"$4"\t"$2"\t"$3}' | /public1/home/sc30797/liuyuxin/software/bedtools2/bin/bedtools merge -i - > maize_Mo17_exon.bed.tmp
